d07ea8e06f914ae86ac63fd021e71bd4184dc6fb
Venkata Rami Reddy <venkatramreddy069@gmail.com>
2025-07-26 07:06:17 +0530

    Loaded all the programs

---
+++ src/com/gqt/corejava/patterns/pattern20.java
@@ -0,0 +1,26 @@
+package com.gqt.corejava.patterns;
+
+import java.util.Scanner;
+
+public class pattern20 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+	      System.out.println("Enter the count:");
+	      int n=sc.nextInt();
+	      int count=1;
+	      for(int i=1;i<=n;i++) {
+	    	  for(int j=1;j<=n;j++) {
+	    	  System.out.print("_");
+	    	 
+	      }
+	    	  for(int j=1;j<=n;j++) {
+		    	  System.out.print("@");
+		    	 
+		      }
+	      System.out.println();
+		}
+
+	}
+}
+
